b582bcda123bd40acb989fece2fc049108974b80
utkarshatp <128125516+utkarshatp@users.noreply.github.com>
2023-04-19 17:25:49 +0530

    adfw

--- RemoveCode.py
+++ RemoveCode.py
@@ -1,7 +1,3 @@
-def removeTag(root, element):
-    for elem in root.findall(".//*"):
-        if element in elem:
-            elem.remove(element)
 
 def removeTag1(root, elementList):
     for element in elementList:
